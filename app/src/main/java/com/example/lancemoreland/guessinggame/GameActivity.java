da099a25e300a945a75f06a2d6f62be568da550e
Lance Moreland <lmoreland1899@gmail.com>
2018-03-15 16:06:32 -0400

    Transfered over random number gen and if/else if statements, enabled the cluetext to show after each guess.

--- app/src/main/java/com/example/lancemoreland/guessinggame/GameActivity.java
+++ app/src/main/java/com/example/lancemoreland/guessinggame/GameActivity.java
@@ -2,12 +2,77 @@ package com.example.lancemoreland.guessinggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    private TextView clueTextView;
+    private Button guessButton;
+    private EditText guess;
+    private int generatedNumber;
+    private int numberOfGuesses = 0;
+    private int MAX_GUESS_COUNT = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        clueTextView = findViewById(R.id.clue_Textview);
+        guessButton = findViewById(R.id.submitNumber_Button);
+        guess = findViewById(R.id.editText);
+
+        setListener();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        generatedNumber = (int) Math.ceil(Math.random() * 100);
+    }
+
+    private void setListener(){
+        guessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int userGuess = Integer.parseInt(guess.getText().toString());
+                if (userGuess > 100){
+                    clueTextView.setText("Enter a number between 1 - 100");
+                    clueTextView.setVisibility(View.VISIBLE);
+                    guess.setText("");
+                }else {
+                    checkGuess(userGuess);
+                }
+            }
+        });
+    }
+
+    private void checkGuess(int userGuess) {
+        if (userGuess < generatedNumber) {
+            clueTextView.setText("Too Low");
+            clueTextView.setVisibility(View.VISIBLE);
+            guess.setText("");
+            numberOfGuesses++;
+
+        } else if (userGuess > generatedNumber) {
+            clueTextView.setText("Too High");
+            clueTextView.setVisibility(View.VISIBLE);
+            guess.setText("");
+            numberOfGuesses++;
+
+        } else if (userGuess == generatedNumber) {
+            //TODO - create intent to go to winning activity - handle winning
+
+        } else if (numberOfGuesses == MAX_GUESS_COUNT) {
+            //TODO - create intent to go to wining activity - out of chances
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
--- app/src/main/java/com/example/lancemoreland/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/lancemoreland/guessinggame/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.lancemoreland.guessinggame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        startButton =findViewById(R.id.start_button);
+        setListener();
+    }
+    private void setListener() {
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
+    }
+    private void startGame(){
+        Intent playGame = new Intent(this, GameActivity.class);
+        startActivity(playGame);
     }
 }
