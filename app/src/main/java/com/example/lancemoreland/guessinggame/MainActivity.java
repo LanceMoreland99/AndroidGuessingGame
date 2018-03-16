3d221d2b06b6290a12780fdb723fe27507254974
Lance Moreland <lmoreland1899@gmail.com>
2018-03-16 16:31:25 -0400

    Added methods for textviews imageview and button, added listener for button

--- app/src/main/java/com/example/lancemoreland/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/lancemoreland/guessinggame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lancemoreland.guessinggame;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_main);
 
         startButton =findViewById(R.id.start_button);
--- app/src/main/java/com/example/lancemoreland/guessinggame/ResultsActivity.java
+++ app/src/main/java/com/example/lancemoreland/guessinggame/ResultsActivity.java
@@ -2,12 +2,39 @@ package com.example.lancemoreland.guessinggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    private Button playAgainButton;
+    private TextView correctNumberTextview;
+    private TextView resultsTextview;
+    private ImageView resultImageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        playAgainButton = findViewById(R.id.restart_button);
+        correctNumberTextview = findViewById(R.id.results_textView);
+        resultsTextview = findViewById(R.id.number_textView);
+        resultImageview = findViewById(R.id.winning_imageView);
+
+        setListener();
     }
+
+    private void setListener() {
+
+        playAgainButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
+    }
+
 }
